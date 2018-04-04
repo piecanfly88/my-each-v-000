56fe57ca0e0235ac98dc76eca91ced89d0ba9a7f
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-04 06:51:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
 def my_each(array)
   counter = 0
   if block_given?
-    yield(array[counter])
-    counter += 1
+    while counter < array.length
+      yield(array[counter])
+      counter += 1
+    end
   end
 end
-
