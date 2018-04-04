8eabafe8abecc36364a4df85a1c356040ea1060c
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-04 06:53:27 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,9 @@
 def my_each(array)
   new_array = []
   counter = 0
-  if block_given?
-    while counter < array.length
-      new_array << yield(array[counter])
-      counter += 1
-    end
+  while counter < array.length
+    new_array << yield(array[counter])
+    counter += 1
   end
   new_array
 end
