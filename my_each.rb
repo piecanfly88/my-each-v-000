b9f00979d6dc7a2ce01ab071cd6d79461b0acfa6
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-04 06:55:56 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)
-  counter = 0
-  while counter < array.length
-    yield(array[counter])
-    counter += 1
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
   end
   array
 end
